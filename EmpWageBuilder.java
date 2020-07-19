7d932ac5e744d32ca9427dc316852bb8bbac4017
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-19 12:45:27 +0530

    Added: Functionality to calculate wages for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,23 +2,29 @@ public class EmpWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int empHrs=0;
 		int empWage=0;
-		int empCheck= (int) Math.floor(Math.random() * 10)%3;
-		switch (empCheck) {
-		    case IS_PART_TIME:
-		        empHrs=4;
-		        break;
-		    case IS_FULL_TIME:
-		        empHrs=8;
-		        break;
-		    default:
-		        empHrs=0;
+		int totalEmpWage=0;
+		for (int day=0; day < NUM_OF_WORKING_DAYS; day++ ) {
+			int empCheck= (int) Math.floor(Math.random() * 10)%3;
+			switch (empCheck) {
+			    case IS_PART_TIME:
+		        	empHrs=4;
+		        	break;
+		    	case IS_FULL_TIME:
+		        	empHrs=8;
+		        	break;
+		    	default:
+		        	empHrs=0;
+			}
+			empWage= empHrs * EMP_RATE_PER_HR;
+			totalEmpWage+=empWage;
+			System.out.println("Employee Wage: " + empWage);
 		}
-		empWage= empHrs * EMP_RATE_PER_HR;
-		System.out.println("Employee Wage: " + empWage);
+		System.out.println("Total Employee Wage: " + totalEmpWage);
 	}
 }
